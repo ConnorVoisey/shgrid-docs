9ce1185b45460951592a8bd7e5e646c934fa996f
Connor Voisey <connor.voisey@gmail.com>
2023-08-28 14:27:20 +0100

    feat(api): added count to contact index route

--- server/src/models/contact.rs
+++ server/src/models/contact.rs
@@ -4,7 +4,6 @@ use crate::error::{Error, Result};
 use axum::{
     debug_handler,
     extract::{Path, Query, State},
-    response::IntoResponse,
     Json,
 };
 use chrono::{offset, DateTime, Utc};
@@ -55,6 +54,10 @@ pub struct ContactFullOutput {
     organisation_id: Option<Uuid>,
     created_at: Option<DateTime<Utc>>,
     updated_at: Option<DateTime<Utc>>,
+}
+#[derive(sqlx::FromRow, Debug, Deserialize, Serialize)]
+pub struct RowCount {
+    count: i64,
 }
 pub async fn create_contacts(
     pool: &Pool<Postgres>,
@@ -115,7 +118,7 @@ fn get_fake_contact_input(organisation_id: Option<Uuid>) -> ContactInput {
     }
 }
 
-fn get_contacts_sql(params: GetContactParams) -> Result<(String, SqlxValues)> {
+fn get_contacts_sql(params: &GetContactParams) -> Result<(String, SqlxValues)> {
     let limit = std::cmp::min(params.limit.unwrap_or(100), 10_000);
     let offset = params.offset.unwrap_or(0);
     let columns = [
@@ -129,53 +132,32 @@ fn get_contacts_sql(params: GetContactParams) -> Result<(String, SqlxValues)> {
         Contact::CreatedAt,
         Contact::UpdatedAt,
     ];
-    let filters = match params.filters {
-        Some(filters) => serde_json::from_str::<Vec<(String, String)>>(&filters)?,
-        None => vec![],
-    };
-    let sorting = match params.sorting {
-        Some(sorting) => serde_json::from_str::<Vec<(String, String)>>(&sorting)?,
-        None => vec![],
-    };
     let mut query = sea_query::Query::select();
     query.from(Contact::Table).columns(columns);
-
-    for filter in filters {
-        let col = match get_contact_col_from_str(&filter.0) {
-            Some(val) => val,
-            None => {
-                return Err(Error::UnprocessableEntity {
-                    errors: HashMap::from([(
-                        "filter".to_owned(),
-                        vec![format!("invalid column name: {}", filter.0)],
-                    )]),
-                });
-            }
-        };
-        let value = filter.1;
-        query.and_where(Expr::col(col).like(&format!("%{value}%")));
-    }
-    dbg!(&sorting);
-    for sorter in sorting {
-        let col = match get_contact_col_from_str(&sorter.0) {
+    add_filters(&mut query, &params.filters)?;
+    for sorter in &params.sorting {
+        let col = match get_contact_col_from_str(&sorter[0]) {
             Some(val) => val,
             None => {
                 return Err(Error::UnprocessableEntity {
                     errors: HashMap::from([(
                         "sorting".to_owned(),
-                        vec![format!("invalid column name: {}", sorter.0)],
+                        vec![format!("invalid column name: {}", sorter[0])],
                     )]),
                 });
             }
         };
-        let direction = match &sorter.1 {
-            _ if &sorter.1 == "asc" => sea_query::Order::Asc,
-            _ if &sorter.1 == "desc" => sea_query::Order::Desc,
+        let direction = match &sorter[1] {
+            _ if &sorter[1] == "asc" => sea_query::Order::Asc,
+            _ if &sorter[1] == "desc" => sea_query::Order::Desc,
             _ => {
                 return Err(Error::UnprocessableEntity {
                     errors: HashMap::from([(
                         "sorting".to_owned(),
-                        vec![format!(r#"invalid direction: {}, expecting "asc" or "desc""#, sorter.1)],
+                        vec![format!(
+                            r#"invalid direction: {}, expecting "asc" or "desc""#,
+                            sorter[1]
+                        )],
                     )]),
                 });
             }
@@ -187,30 +169,105 @@ fn get_contacts_sql(params: GetContactParams) -> Result<(String, SqlxValues)> {
         .offset(offset)
         .build_sqlx(PostgresQueryBuilder {}))
 }
-async fn get_contacts(
-    pool: &Pool<Postgres>,
-    params: GetContactParams,
-) -> Result<Vec<ContactFullOutput>> {
-    let (sql, values) = get_contacts_sql(params)?;
-    Ok(sqlx::query_as_with::<_, ContactFullOutput, _>(&sql, values)
+
+fn get_contact_count_sql(filters: &Filters) -> Result<(String, SqlxValues)> {
+    let mut query = sea_query::Query::select();
+    query
+        .expr(Expr::col((Contact::Table, Contact::Id)).count())
+        .from(Contact::Table);
+
+    add_filters(&mut query, filters)?;
+
+    Ok(query.build_sqlx(PostgresQueryBuilder {}))
+}
+
+fn add_filters<'a>(
+    query: &'a mut sea_query::SelectStatement,
+    filters: &Filters,
+) -> Result<&'a mut sea_query::SelectStatement> {
+    for filter in filters {
+        let col = match get_contact_col_from_str(&filter[0]) {
+            Some(val) => val,
+            None => {
+                return Err(Error::UnprocessableEntity {
+                    errors: HashMap::from([(
+                        "filter".to_owned(),
+                        vec![format!("invalid column name: {}", filter[0])],
+                    )]),
+                });
+            }
+        };
+        let value = &filter[1];
+        query.and_where(Expr::col(col).like(&format!("%{value}%")));
+    }
+    Ok(query)
+}
+
+#[derive(Serialize)]
+pub struct ContactResponse {
+    count: i64,
+    data: Vec<ContactFullOutput>,
+}
+
+async fn get_contacts(pool: &Pool<Postgres>, params: GetContactParams) -> Result<ContactResponse> {
+    let (sql, values) = get_contacts_sql(&params)?;
+    let contacts = sqlx::query_as_with::<_, ContactFullOutput, _>(&sql, values)
         .fetch_all(pool)
-        .await?)
+        .await?;
+
+    let (sql, values) = get_contact_count_sql(&params.filters)?;
+    let count = sqlx::query_as_with::<_, RowCount, _>(&sql, values)
+        .fetch_all(pool)
+        .await?[0]
+        .count;
+
+    Ok(ContactResponse {
+        count,
+        data: contacts,
+    })
 }
 
 #[derive(Deserialize, Debug)]
-pub struct GetContactParams {
+pub struct GetContactParamsPreSerialize {
     limit: Option<u64>,
     offset: Option<u64>,
     sorting: Option<String>,
     filters: Option<String>,
 }
-
+#[derive(Deserialize, Debug)]
+pub struct GetContactParams {
+    limit: Option<u64>,
+    offset: Option<u64>,
+    sorting: Sorting,
+    filters: Filters,
+}
+type Filters = Vec<[String; 2]>;
+type Sorting = Vec<[String; 2]>;
+impl GetContactParams {
+    pub fn from(pre_serialize: GetContactParamsPreSerialize) -> Result<GetContactParams> {
+        let filters = match pre_serialize.filters {
+            Some(filters) => serde_json::from_str::<Filters>(&filters)?,
+            None => vec![],
+        };
+        let sorting = match pre_serialize.sorting {
+            Some(sorting) => serde_json::from_str::<Sorting>(&sorting)?,
+            None => vec![],
+        };
+        Ok(GetContactParams {
+            limit: pre_serialize.limit,
+            offset: pre_serialize.offset,
+            filters,
+            sorting,
+        })
+    }
+}
 #[debug_handler]
 pub async fn index_contact(
     State(pool): State<Pool<Postgres>>,
-    Query(params): Query<GetContactParams>,
-) -> Result<Json<Vec<ContactFullOutput>>> {
-    let contacts_res = get_contacts(&pool, params).await?;
+    Query(params): Query<GetContactParamsPreSerialize>,
+) -> Result<Json<ContactResponse>> {
+    let params_serialized = GetContactParams::from(params)?;
+    let contacts_res = get_contacts(&pool, params_serialized).await?;
     Ok(Json(contacts_res))
 }
 
@@ -228,12 +285,13 @@ mod test {
 
     #[test]
     fn select_contacts_sql_simple() {
-        let params = GetContactParams {
+        let params = GetContactParams::from(GetContactParamsPreSerialize {
             offset: Some(200),
             limit: Some(50),
             sorting: None,
             filters: None,
-        };
+        })
+        .unwrap();
         let (sql, _) = get_contacts_sql(params).unwrap();
         assert_eq!(
             sql,
